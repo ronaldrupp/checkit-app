a3475846a9f026bc92ca3418f85bdfde415961af
Ronald Rupp <ronald.rupp@outlook.at>
2020-10-29 19:14:46 +0100

    implemented Vuex and cleaned up vue starter templ.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>Vuex: {{ $store.state.user }}</h1>
+    <input type="text" v-model="userName" @keyup="setUser"/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      userName: "Tess Tosterone",
+    };
+  },
+  created() {
+    this.setUser();
+  },
+  methods: {
+    setUser() {
+      this.$store.dispatch("setUser", this.userName);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {},
   },
   mutations: {
+    setUser(state, value) {
+      this.state.user = value;
+    },
   },
   actions: {
+    setUser(context, value) {
+      context.commit("setUser", value);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
