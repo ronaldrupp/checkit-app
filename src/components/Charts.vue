ba33ad933c9d6f3a65e10d1b72da9cd6300017bf
Pascal Rengelshausen <rengelshausen.p01@htl-ottakring.ac.at>
2021-01-27 11:59:30 +0100

    installed chart.js and implementation started

---
+++ src/components/Charts.vue
@@ -0,0 +1,38 @@
+<script>
+import { Pie } from "vue-chartjs";
+export default {
+  data() {
+    return {
+      data: {
+        labels: ["Ja", "Nein", "Vielleicht"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    };
+  },
+  extends: Pie,
+  mounted() {
+    this.renderChart(this.data, this.options);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/FeedbackDetail.vue
+++ src/views/FeedbackDetail.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    <Header :back="true" :title="$route.params.feedback"></Header>
-    FeedbackDetail
+  <div class="overflow-y-scroll pb-20">
+    <div class="flex flex-col">
+      <Header :back="true" :title="$route.params.feedback"></Header>
+      <div
+        class="flex flex-col border rounded-md m-5 p-5 justify-between md:items-start border-b transition duration-100 dark:border-gray-800 "
+      >
+      <p class="text-lg">Sind Sie dumm?</p>
+        <div class="max-w-md">
+          <Charts />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "./../components/Header";
+import Charts from "@/components/Charts.vue";
 export default {
   components: {
     Header,
+    Charts,
+  },
+  data() {
+    return {};
   },
 };
 </script>
